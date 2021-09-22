e1f5dd56631030e1431777912931228bb4d086e7
Gulnaz <88134441+GulnazSharshenalieva@users.noreply.github.com>
2021-09-22 23:04:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+
+// LESSON 32, JS#4, TASK#1
+// let gardenLength = prompt("Please enter your garden length in feet:");
+// let gardenWidth = prompt("Please enter your garden width in feet:");
+// const acre = 43560;
+// let gardenArea = gardenLength * gardenWidth;
+// document.write(`<b>Your garden area in acres is:</b> ${(gardenArea / acre).toFixed(2)}`);
+
+
+// LESSON 32, JS#4, TASK#2
+
+// let BottleQtyLess1Lt = prompt("Please enter the quantity of bottle 1 liter or less:");
+
+// let BottleQtyMore1Lt = prompt("Please enter the quantity of bottle more than 1 liter:");
+
+// const Bottle1Orless = 0.10;
+
+// const BottleMore1 = 0.25;
+
+// document.write(`<b>Your total amount is</b>: $${(BottleQtyLess1Lt * Bottle1Orless + BottleQtyMore1Lt * BottleMore1).toFixed(2)}`);
+
+
+// LESSON 32, JS#4, TASK#3
+// let yourOrderSum = prompt("Please enter your order total:");
+// 
+// 
+// const taxOrder = ((yourOrderSum * 18) / 100).toFixed(2);
+// const serviceOrder = ((yourOrderSum * 18) / 100).toFixed(2);
+// document.write(`<b>Tax:</b> $${taxOrder}` + "<br>");
+// document.write(`<b>Service:</b> $${serviceOrder}` + "<br>");
+// document.write(`<b>Total:</b> $${taxOrder + serviceOrder + yourOrderSum`);
+
+
+// LESSON 32, JS#4, TASK#4
+// let n = prompt("Please enter any number:");
+// let sum = n * (n + 1) / 2;
+// document.write()
+
+
+// LESSON 32, JS#4, TASK#5
+// let souvenirQty = prompt("Please enter souvenirs quantity:");
+// let bijouQty = prompt("Please enter bijou quantity:");
+// const souvenirWeight = 75;
+// const bijouWeight = 112;
+// let result = souvenirQty * souvenirWeight + bijouQty * bijouWeight;
+// document.write("<b>Total weight:</b>" + result + "gr");
+
+// LESSON 32, JS#4, TASK#6
+// let customerDeposite = +prompt("Please enter your total deposite:");
+// const firstYearTotal = (customerDeposite * 4) / 100 + customerDeposite;
+// const secondYearTotal = (firstYearTotal * 4)/ 100 + customerDeposite;
+// const thirdYearTotal = (secondYearTotal * 4) / 100 + customerDeposite;
+// document.write(`<b>1st year total:</b>$${(firstYearTotal).toFixed(2)}<br>`);
+// document.write(`<b>2nd year total:</b>$${(secondYearTotal).toFixed(2)}<br>`);
+// document.write(`<b>3rd year total:</b>$${(thirdYearTotal).toFixed(2)}`);
+
+
+// LESSON 32, JS#4, TASK#7
+
+// let a = +prompt("Please enter your first number:");
+
+// let b = +prompt("Please enter your second number");
+
+// let c = a - b;
+// let d = a / b;
+// let e = a * b;
+// let f = a % b;
+// let j = a ** b;
+// let h = (a + b);
+
+// console.log(`The difference is: ${c}`);
+// console.log(`The division is: ${d}`);
+// console.log(`The multiplication is: ${e}`);
+// console.log(`The remainder is: ${f}`);
+// console.log(`The exponentiation is: ${j}`);
+// console.log(`The sum is: ${h}`);
+
+
+
+
